75cfc5bec3eb49e3b365ef9aadaba8f7882b6526
Myloi Mellanc <mellanc@Myloiui-MacBook-Air.local>
2017-11-07 22:44:56 +0900

    Initial Commit

---
+++ Pensiv/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pensiv
+//
+//  Created by Myloi Mellanc on 2017. 11. 7..
+//  Copyright © 2017년 MyloiMellanc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
