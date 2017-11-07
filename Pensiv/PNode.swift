d38c19f5641f32af6e09bf29b09b70a217fbf11b
Myloi Mellanc <mellanc@Myloiui-MacBook-Air.local>
2017-11-07 23:40:35 +0900

    First commit with basic source file.

---
+++ Pensiv/PNode.swift
@@ -0,0 +1,24 @@
+//
+//  PNode.swift
+//  Pensiv
+//
+//  Created by Myloi Mellanc on 2017. 11. 7..
+//  Copyright © 2017년 MyloiMellanc. All rights reserved.
+//
+
+import Foundation
+
+
+class PNode
+{
+    //ID
+    //Position
+    //Scale
+    //TextSize
+    //Alpha
+    //Color
+    //Zorder
+    //
+}
+
+
---
+++ Pensiv/PTouchManager.swift
@@ -0,0 +1,9 @@
+//
+//  PTouchManager.swift
+//  Pensiv
+//
+//  Created by Myloi Mellanc on 2017. 11. 7..
+//  Copyright © 2017년 MyloiMellanc. All rights reserved.
+//
+
+import Foundation
